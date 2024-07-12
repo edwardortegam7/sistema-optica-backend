796d8f5a3aeba86792c6a030bcf9fcdbc2672659
kenneth <126686075+KennethM19@users.noreply.github.com>
2024-07-11 22:43:21 -0500

    kenneth (#4)

--- src/main/java/com/sistema/optica/servicios/ClienteService.java
+++ src/main/java/com/sistema/optica/servicios/ClienteService.java
@@ -4,5 +4,6 @@ import com.sistema.optica.entidades.Cliente;
 
 public interface ClienteService {
     public Cliente guardarCliente(Cliente cliente) throws Exception;
-    public Cliente obtenerCliente(String dni);
+    public Cliente obtenerCliente(String username);
+    public Cliente obtenerClienteId(Long id);
 }
